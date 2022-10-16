5c874e311d9fa0b07d5fd852332c73d212fd48a8
reioto <reioto77@gmail.com>
2022-10-16 18:07:34 +0900

    feat: Add Sample Class

---
+++ tests/SampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NAMESPACE;
+
+use PHPUnit\Framework\TestCase;
+
+class SampleTest extends TestCase
+{
+    public function testSample(): void
+    {
+        $this->assertTrue(true);
+    }
+}
